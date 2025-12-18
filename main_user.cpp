7e91deb08cd9a7f595d5725a6b5046a725d17a5d
Azmazey <azmasyabila@gmail.com>
2025-12-18 22:21:11 +0700

    Update main_user.cpp

--- main_user.cpp
+++ main_user.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "main.h"
+#include "relation.h"
+
 using namespace std;
 
 void menuUser(listBis &LB, listPenumpang &LP, listRelasi &LR){
@@ -8,9 +10,10 @@ void menuUser(listBis &LB, listPenumpang &LP, listRelasi &LR){
     while(pilih != 0){
         system("cls");
         cout << "===== MENU USER =====\n";
-        cout << "1. Lihat Bis\n";
-        cout << "2. Lihat Penumpang\n";
-        cout << "3. Lihat Relasi\n";
+        cout << "1. Lihat Semua Bis\n";
+        cout << "2. Lihat Semua Penumpang\n";
+        cout << "3. Lihat Relasi Bis ke Penumpang\n";
+        cout << "4. Lihat Relasi Penumpang ke Bis\n";
         cout << "0. Back\n";
         cout << "Pilih: ";
         cin >> pilih;
@@ -24,7 +27,11 @@ void menuUser(listBis &LB, listPenumpang &LP, listRelasi &LR){
             system("pause");
         }
         else if(pilih == 3){
-            showAllRelasi(LR);
+            showRelasiBisKePenumpang(LB, LR);
+            system("pause");
+        }
+        else if(pilih == 4){
+            showRelasiPenumpangKeBis(LP, LR);
             system("pause");
         }
     }
